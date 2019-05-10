57c58006fdb3a3648db96013682abd4d5f2a8d6a
CateRios <caterinarios@gmail.com>
2019-05-10 08:29:17 +0100

    Generado layout

--- app/src/main/java/es/ulpgc/caterina/rios101/control_3/main/MainActivity.java
+++ app/src/main/java/es/ulpgc/caterina/rios101/control_3/main/MainActivity.java
@@ -41,6 +41,6 @@ public class MainActivity
     //Log.e(TAG, "displayData()");
 
     // deal with the data
-    ((TextView) findViewById(R.id.data)).setText(viewModel.data);
+//    ((TextView) findViewById(R.id.data)).setText(viewModel.data);
   }
 }
